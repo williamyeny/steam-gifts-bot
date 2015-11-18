a7d66ab546bfe3b8f1d08924a75d9e29bfbc8f29
William Ye <williamyeny@gmail.com>
2015-11-18 20:59:55 +0000

    updated ajax.php

--- steamgifts.rb
+++ steamgifts.rb
@@ -4,28 +4,39 @@ mechanize = Mechanize.new
 
 puts "Loading cookies..."
 mechanize.cookie_jar.load 'cookies'
+puts "Cookies loaded!"
+puts
+while true
+  begin
+    page = mechanize.get('http://www.steamgifts.com/')
+    points = page.search('.nav__points').text.strip.to_i
+    if points >= 230
+      pageNumber = 0
+      while points > 5
+        pageNumber += 1
+        page = mechanize.get("http://www.steamgifts.com/giveaways/search?page=" + pageNumber.to_s);
 
-for i in 1..5
-  page = mechanize.get("http://www.steamgifts.com/giveaways/search?page=" + i.to_s);
-
-  page.search(".giveaway__heading__name").each do |x|
-    giveaway = Mechanize::Page::Link.new( x, mechanize, page ).click
-    form = giveaway.forms.first
-    begin
-      if form['code'] != nil
-        puts 'title: ' + giveaway.title
-        puts 'token: ' + form['xsrf_token']
-        puts 'code: ' + form['code']
-        mechanize.post('http://www.steamgifts.com/ajax_2J2No.php', {
-          xsrf_token: form['xsrf_token'],
-          do: "entry_insert",
-          code: form['code']
-        })
-        puts 'entered!'
-        puts
+        page.search(".giveaway__heading__name").each do |x|
+          giveaway = Mechanize::Page::Link.new( x, mechanize, page ).click
+          form = giveaway.forms.first
+          if form['code'] != nil
+            print giveaway.title + " [" + form['code'] + "] | "
+            mechanize.post('http://www.steamgifts.com/ajax.php', {
+              xsrf_token: form['xsrf_token'],
+              do: "entry_insert",
+              code: form['code']
+            })
+            puts "Points left: #{page.search('.nav__points').text.strip}"
+          end
+        end
+        points = page.search('.nav__points').text.strip.to_i
       end
-    rescue
-      puts 'Oh no! Something happened when trying to enter \'' + giveaway.title + '\'!'
+    else
+      puts "Not enough points: " + points.to_s
     end
+  rescue
+    puts "Something happened! D:"
   end
+  sleep(60)
+  
 end
